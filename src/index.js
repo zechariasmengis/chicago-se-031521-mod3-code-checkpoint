9c7fbfa888b2ceffb6ab1d2cdbddddc735e076ee
Zecharias Mengis <zmengis@gmail.com>
2021-05-05 15:20:24 -0500

    more stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const imageApi = 'http://localhost:3000'
+const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  };
 
 const commentSection = document.getElementsByClassName('comments')[0];
 const imageCard = document.getElementsByClassName('image')[0];
 const title = document.getElementsByClassName('title')[0];
+const likeCount = document.querySelector('.likes')
 
 fetch(`${imageApi}/images/1`).then((res) => res.json()).then(displayPost);
 
@@ -13,6 +18,21 @@ function displayPost(image) {
 
     imageCard.src = image.image;
     title.innerHTML = `${image.title}`;
+    likeCount.innerHTML = `${image.likes} likes`
+
+    document.querySelector('.like-button').addEventListener('click', (e) => {
+        // when the button is clicked we're making a fetch to the database
+        fetch(`${imageApi}/images/1`, {
+            method: 'PATCH',
+            headers,
+            // we're incrementing the likes on the image by 1 and then translating that to json
+            body: JSON.stringify({likes: image.likes + 1}),
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                image.likes = json.likes;
+            })
+    })
 }
 
 function showComment(comment) {
