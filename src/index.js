6b11e5058c7b19afdaa3f120a8979670a75fe75c
Zecharias Mengis <zmengis@gmail.com>
2021-05-05 11:25:43 -0500

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// write your code here
+const imageApi = 'http://localhost:3000'
+
+const commentSection = document.getElementsByClassName('comments')[0];
+const imageCard = document.getElementsByClassName('image')[0];
+const title = document.getElementsByClassName('title')[0];
+
+fetch(`${imageApi}/images/1`).then((res) => res.json()).then(displayPost);
+
+function displayPost(image) {
+    const comments = image.comments;
+
+    comments.forEach(showComment);
+
+    imageCard.src = image.image;
+    title.innerHTML = `${image.title}`;
+}
+
+function showComment(comment) {
+    const newComment = document.createElement('li');
+    newComment.innerHTML = `<p> ${comment.content} </p>`;
+    commentSection.append(newComment);
+}
